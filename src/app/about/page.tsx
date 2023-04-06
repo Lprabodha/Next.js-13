4a3330bcb4eded98a40557a3a3d3d811b23a3a77
Lprabodha <lprabodha98@gmail.com>
2023-04-06 18:34:35 +0530

    project setup

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+
+        <h1>About Rightmo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, debitis ut excepturi dolorem vero exercitationem quo, repellendus officia amet, dolores quos! Modi quas deserunt, ducimus ratione atque reiciendis sed vel!</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/app/about/team/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+        <h1>Our Team</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt laborum quaerat nam doloremque placeat delectus odit fuga mollitia necessitatibus, voluptates repellendus architecto nostrum recusandae reiciendis maxime fugit dolorem repellat dicta soluta, praesentium corrupti adipisci in? Reiciendis temporibus magni at porro, velit non nemo saepe praesentium eveniet optio libero natus dicta vitae facere ipsam sit consequuntur! Velit, maxime recusandae. Enim iste molestiae quos ullam doloribus aliquid neque saepe quidem possimus atque nam, nulla molestias fuga sequi repellendus dicta error placeat eveniet modi totam dolorum tempora. Animi, reprehenderit quia totam doloribus pariatur blanditiis voluptatibus perferendis accusantium nulla. Voluptatem libero eveniet soluta laboriosam?</p>
+    </div>
+  )
+}
+
+export default TeamPage

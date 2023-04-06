c43f8e134860b37ded4d5b1a37733268e951cd74
Lprabodha <lprabodha98@gmail.com>
2023-04-06 19:06:28 +0530

    header componts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,29 @@
-import { Poppins } from 'next/font/google';
-import './globals.css'
+import { Poppins } from "next/font/google";
+import Header from "./components/Header";
+import "./globals.css";
 
-const poppins =  Poppins({
-  weight : ['400', '700'],
-  subsets : ['latin']
+const poppins = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
 });
 
 export const metadata = {
-  title: 'Rightmo Web',
-  description: 'Web Development company',
-  keywords: 'Rightmo',
-}
+  title: "Rightmo Web",
+  description: "Web Development company",
+  keywords: "Rightmo",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Header/>
+        <main className="container">{children}</main>
+      </body>
     </html>
-  )
+  );
 }

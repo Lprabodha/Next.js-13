0714c82a7df23357efd94cf2e3108e44097a66c5
Lprabodha <lprabodha98@gmail.com>
2023-04-07 21:00:05 +0530

    Fetching Course Data

--- src/app/components/Courses.tsx
+++ src/app/components/Courses.tsx
@@ -1,8 +1,26 @@
+import Link from "next/link";
 
-const Courses = () => {
-  return (
-    <div>Courses</div>
-  )
+async function fetchCourses() {
+  const response = await fetch(`http://localhost:3000/api/courses`);
+  const courses = await response.json();
+  return courses;
 }
 
-export default Courses
+const Courses = async () => {
+  const courses  = await fetchCourses();
+  return (
+    <div className="courses">
+      {courses.map((course) => (
+        <div key={course.id} className="card">
+          <h2>{course.title}</h2>
+          <small>Level: {course.level}</small>
+          <p>{course.description}</p>
+          <Link href={course.link} target="_blank" className="btn">
+            Go To Course
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Courses;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Link from 'next/link'
+import Courses from './components/Courses'
 
 const HomePage = () => {
   return (
     <div>
 
       <h1>Welcome to Rightmo</h1>
-      <ul>
-        <li><Link href='/'>Home</Link></li>
-        <li><Link href='/about'>About</Link></li>
-        <li><Link href='/about/team'>Team</Link></li>
-      </ul>
+      <Courses/>
+   
     </div>
   )
 }

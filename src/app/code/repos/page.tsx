c89fbaba18ec5ab66cba708b1c6c7082bb7193d9
Lprabodha <lprabodha98@gmail.com>
2023-04-07 19:57:33 +0530

    Caching & Revalidating

--- src/app/code/repos/page.tsx
+++ src/app/code/repos/page.tsx
@@ -3,7 +3,11 @@ import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
 
 
 async function fetchRepos() {
-  const response = await fetch("https://api.github.com/users/Lprabodha/repos");
+  const response = await fetch("https://api.github.com/users/Lprabodha/repos", {
+    next : {
+      revalidate : 60
+    }
+  });
 
   await new Promise((resolve) => setTimeout(resolve,1000)); // wait 1 seconds
 

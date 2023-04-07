b5d39ce926988df07dff49279ff7d45aa5f6ca49
Lprabodha <lprabodha98@gmail.com>
2023-04-07 17:34:28 +0530

    Repo & RepoDir Component

--- src/app/code/repos/[name]/page.tsx
+++ src/app/code/repos/[name]/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Repo from "@/app/components/Repo";
+import RepoDirs from "@/app/components/RepoDirs";
 
 const RepoPage = ({ params: { name } }) => {
   return (
     <div className="card">
       <Link href="/code/repos" className="btn btn-back"> Back to Repositories</Link>
       <Repo name={name} />
+      <RepoDirs name={name}/>
     </div>
   );
 };
